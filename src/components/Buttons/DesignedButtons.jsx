d7d5ff074adcc5787cb8a3434624ba6ed44e4174
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-10-09 13:38:12 -0400

    Built out DesignedButtons
    - TertiaryButton and SecondaryButton seem to be working properly
    
    - PrimaryButton is not receiving the box-shadow at this time.
    - Still need to add in the correct font based on the Figma file
    - Padding is pushing words out of the box at the current fixed widths
    	- Could look into using min-width instead

---
+++ src/components/Buttons/DesignedButtons.jsx
@@ -0,0 +1,19 @@
+﻿import "./DesignedButtons.css";
+
+export function PrimaryButton({ text }) {
+	return (
+		<button className="primary-btn designed-btn">{text}</button>
+	);
+}
+
+export function SecondaryButton({ text }) {
+	return (
+		<button className="secondary-btn designed-btn">{text}</button>
+	);
+}
+
+export function TertiaryButton({ text }) {
+	return (
+		<button className="tertiary-btn designed-btn">{text}</button>
+	);
+}
