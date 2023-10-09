a1d5e6e3810ca0e3119483bb93aae9b75f8010f2
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-10-09 15:13:52 -0400

    Updated DesignedButtons and created fonts folder
    - fonts folder will hold the needed fonts for site
            - currently imported Poppins font
    
    - DesignedButtons.css updates
            - Added @font-face for Poppins
            - primary-btn, secondary-btn, and tertiary-btn updates
                    - Added font-family for Poppins / sans-serif
                    - Updated color with opacity for the box-shadow
                    - Added borders based on which class
            - Removed designed-btn class as this was not being utilized
            - Organized DesignedButtons.css based on Normal, Hover, and Active State
    
    - DesignedButtons.jsx updates
    	- removed designed-btn className from each exported button
    
    - box-shadow is now showing for PrimaryButton

--- src/components/Buttons/DesignedButtons.jsx
+++ src/components/Buttons/DesignedButtons.jsx
@@ -1,19 +1,13 @@
 ﻿import "./DesignedButtons.css";
 
 export function PrimaryButton({ text }) {
-	return (
-		<button className="primary-btn designed-btn">{text}</button>
-	);
+	return <button className="primary-btn">{text}</button>;
 }
 
 export function SecondaryButton({ text }) {
-	return (
-		<button className="secondary-btn designed-btn">{text}</button>
-	);
+	return <button className="secondary-btn">{text}</button>;
 }
 
 export function TertiaryButton({ text }) {
-	return (
-		<button className="tertiary-btn designed-btn">{text}</button>
-	);
+	return <button className="tertiary-btn">{text}</button>;
 }
