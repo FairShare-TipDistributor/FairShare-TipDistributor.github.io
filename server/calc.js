440c8fbe3c0305fc28ec31a761df158757962533
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-10-17 17:32:10 -0500

    finished calc.js which will get called in route to return array of objects with each employee's share of tips to insert into DB

--- server/calc.js
+++ server/calc.js
@@ -1,21 +1,44 @@
 
-
-
-// share = totalTips/totalHoursWorked x individualHoursWorked
+// Test data. This will come from client in deployed version
 let tipsTotal = 300;
-let hoursTotal = 40;
 let nameAndHours = [
     {name:'Tim', hours:20},
     {name:'Tod', hours:20},
 ]
 
+
+/**
+ * 
+ * @param {*} tipsTotal Total amount of tips from client
+ * @param {*} nameAndHours array of objects containing employee name and hours worked
+ * @returns Returns object with employee names and their share of tips
+ */
 function shareOfTips (tipsTotal, nameAndHours) {
-    let employeeShare = [];
-    let hoursTotal;
-    for (let hours of nameAndHours) {
-        hoursTotal += nameAndHours.hours
+    /**
+     * 
+     * @param {number} hoursTotal total # of hours worked (derived in for loop)
+     * @param {number} empHours hours the individual employee worked
+     * @param {*} tipsTotal Total # of tips from input on client
+     * @returns Returns number amount of each employees' share of tips
+     */
+    function calcTip (hoursTotal, empHours, tipsTotal) {
+        let empShare = tipsTotal/hoursTotal * empHours
+        return empShare
     }
-    return hoursTotal;
+    let employeeShare = [];
+    let singleEmp = {};
+    let hoursTotal = 0;
+    // gets total hours from employees
+    for (let employeeHours of nameAndHours) {
+        hoursTotal += employeeHours.hours;
+    }
+    for (let employeeTips of nameAndHours) {
+        singleEmp.name = employeeTips.name;
+        singleEmp.tips = calcTip(hoursTotal, employeeTips.hours, tipsTotal);
+        employeeShare.push(singleEmp);
+    }
+    return employeeShare
 }
 
 console.log(shareOfTips(tipsTotal, nameAndHours));
+
