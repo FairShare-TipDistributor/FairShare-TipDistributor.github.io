93b7bb225f292be0884fe35f29f5cf593c133c9d
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-10-17 18:21:18 -0500

    initial SQL code for GET

--- server/routes/tips.router.js
+++ server/routes/tips.router.js
@@ -7,6 +7,18 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+  const queryText = `
+  SELECT "name", "share", "tip_total", "tips_date"."date"
+  FROM "tips"
+  JOIN "tips_date" ON "tips"."date_id" = "tips_date"."id"
+  ORDER BY "tips_date"."date";
+  `;
+  pool.query(queryText).then(result => {
+    res.send(result.rows);
+  }).catch(error => {
+    console.log(`Error in GET tipsRouter ${error}`);
+    res.sendStatus(500);
+  });
 });
 
 /**
