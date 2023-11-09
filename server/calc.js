bb1834938c475ee1766b5431c81474026dae8ff6
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-11-09 10:32:00 -0600

    current final version of calc function done

--- server/calc.js
+++ server/calc.js
@@ -3,13 +3,13 @@ const Dinero = require('dinero.js');
 
 // Test data. This will come from client in deployed version
 //! DINERO AMOUNTS ARE IN CENTS
-const tipsTotal = Dinero({amount: 666223, currency:'USD'});
+const tipsTotal = Dinero({amount: 10000, currency:'USD'});
 let nameAndHours = [
-    {name:'Tim', hours:3},
-    {name:'Allen', hours:3},
-    {name:'Bob', hours:3},
-    {name: 'Lenny', hours: 3},
-    {name: 'Brendan', hours: 3},
+    {name:'Tim', id: 1, hours:3},
+    {name:'Allen', id: 2, hours:2},
+    {name:'Bob', id: 3, hours:4},
+    {name: 'Lenny', id: 4, hours: 10},
+    {name: 'Brendan', id: 5, hours: 1.6},
 ];
 
 /**
@@ -19,43 +19,40 @@ let nameAndHours = [
  * @returns Returns object with employee names and their share of tips
  */
 function shareOfTips (tipsTotal, nameAndHours) {
-    let employeeShare = [];
     let hoursTotal = 0;
+    // Shuffles array to give remainder randomly
     let shuffledNameAndHours = nameAndHours.sort( () => Math.random() - 0.5);
 
     // gets total hours from employees
-    for (let employeeHours of nameAndHours) {
+    for (let employeeHours of shuffledNameAndHours) {
         hoursTotal += employeeHours.hours;
     }
+    // Adds total hours to array of objects
+    shuffledNameAndHours.totalHours =  hoursTotal;
 
     // adds each employee and their share of total tips to an array
-    for (let employeeTips of shuffledNameAndHours) {
-        employeeShare.push({
-            name: employeeTips.name,
-            tips: calcTip(hoursTotal, employeeTips.hours, tipsTotal)
-        });
-    }
-    return employeeShare
-}
-
-function calcTip (hoursTotal) {
     let allocations = [];
     let i = 0;
     // Adds each employee's time as percentage of total time to allocations array
-    for (let employee of nameAndHours) {
+    for (let employee of shuffledNameAndHours) {
         allocations.push(employee.hours/hoursTotal * 100);
     }
     // Allocates tips based on percentage of total hours
     let payout = tipsTotal.allocate(allocations);
-
+    
     // Adds payout to nameAndHours
     while (i < payout.length) {
-        nameAndHours[i].share = payout[i].getAmount();
+        nameAndHours[i].share = payout[i].getAmount()/100;
         i++;
     }
+        console.log(shuffledNameAndHours);
+    
+    return shuffledNameAndHours;
 }
 
-calcTip(15);
+shareOfTips(tipsTotal, nameAndHours);
 
-// console.log(nameAndHours.sort( () => Math.random() - 0.5) );
+
+
+
 
