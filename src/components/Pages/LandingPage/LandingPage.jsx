1d47cf58678f1283eebe44df8a90db63857a5132
Joshua Engebretson <joshua.engebretson@gmail.com>
2024-01-19 16:50:00 -0600

    Started to tweak LandingPage layout to line up with Figma file
    - LandingPage changes
    	- Added in Primary, Secondary, and TertiaryButton
    	- Adjusted background of LandingPage to $color-grey-light
    	- Adjusted margins for grid to 17vh auto
    	- Adjusted min-height and width to be 66vh and 80vw
    
    - LoginForm changes
    	- Set up a useState to track if the password should be hidden or visible
    	- Set up a clickable element "Show" to change the state on if the password is hidden or visible
    
    - LoginAndRegister changes
    	- Updated to adjusting .login-input instead of all input elements
    		- Updated background-color to $color-white
    		- Set width to 100% of available space (40% of the screen currently)

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -5,6 +5,11 @@ import "./LandingPage.css";
 // CUSTOM COMPONENTS
 import RegisterForm from "../LoginAndRegisterPages/RegisterPage/RegisterForm/RegisterForm";
 import LoginForm from "../LoginAndRegisterPages/LoginPage/LoginForm/LoginForm";
+import {
+	PrimaryButton,
+	SecondaryButton,
+	TertiaryButton,
+} from "../../Buttons/DesignedButtons";
 
 function LandingPage() {
 	const [heading, setHeading] = useState("Welcome to Fair$hare!");
@@ -15,23 +20,28 @@ function LandingPage() {
 	};
 
 	return (
-		<div className="container">
-			<h2></h2>
-
+		<div className="container landing-background">
 			<div className="grid">
-				<div className="grid-col grid-col_8"></div>
-				<div className="grid-col grid-col_4">
+				<div className="grid-col grid-col_6">test 6</div>
+				<div className="grid-col grid-col_6">
 					<LoginForm />
 
 					<center>
-						<h4>Not a Member?</h4>
-						<button
-							className="btn btn_sizeSm"
-							onClick={registration}
-						>
-							Register
-						</button>
+						<div>
+							<PrimaryButton
+								text="Log In"
+								onClick={registration}
+							/>
+						</div>
 					</center>
+					<div>
+						<a href="#">Forgot User ID or Password?</a>
+					</div>
+
+					<SecondaryButton
+						text="New User?"
+						onClick={registration}
+					/>
 				</div>
 			</div>
 		</div>
--- src/components/Pages/LoginAndRegisterPages/LoginPage/LoginForm/LoginForm.jsx
+++ src/components/Pages/LoginAndRegisterPages/LoginPage/LoginForm/LoginForm.jsx
@@ -1,63 +1,94 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const errors = useSelector(store => store.errors);
-  const dispatch = useDispatch();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [showHidePassword, setShowHidePassword] = useState(false);
+	const errors = useSelector((store) => store.errors);
+	const dispatch = useDispatch();
 
-  const login = (event) => {
-    event.preventDefault();
-    if (username && password) {
-      dispatch({
-        type: 'LOGIN',
-        payload: {
-          username: username,
-          password: password,
-        },
-      });
-    } else {
-      dispatch({ type: 'LOGIN_INPUT_ERROR' });
-    }
-  }; // end login
+	const login = (event) => {
+		event.preventDefault();
+		if (username && password) {
+			dispatch({
+				type: "LOGIN",
+				payload: {
+					username: username,
+					password: password,
+				},
+			});
+		} else {
+			dispatch({ type: "LOGIN_INPUT_ERROR" });
+		}
+	}; // end login
 
-  return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
-  );
+	const showPassword = () => {
+		setShowHidePassword(!showHidePassword);
+	};
+
+	let passwordState;
+
+	if (showHidePassword === true) {
+		passwordState = "text";
+	} else {
+		passwordState = "password";
+	}
+
+	return (
+		<form className="formPanel" onSubmit={login}>
+			<h2>Login</h2>
+			{errors.loginMessage && (
+				<h3 className="alert" role="alert">
+					{errors.loginMessage}
+				</h3>
+			)}
+			<div>
+				<label htmlFor="username">User ID</label>
+				<div />
+				<input
+					className="login-input"
+					type="text"
+					name="username"
+					required
+					value={username}
+					onChange={(event) =>
+						setUsername(event.target.value)
+					}
+				/>
+			</div>
+			<div>
+				<label htmlFor="password">
+					Password
+					<div>
+						Must be at least 8 characters long and include
+						one capital letter and one letter.
+					</div>
+				</label>
+				<input
+					className="login-input"
+					type={passwordState}
+					name="password"
+					id="password"
+					required
+					value={password}
+					onChange={(event) =>
+						setPassword(event.target.value)
+					}
+				/>
+				<p id="show" onClick={showPassword}>
+					Show
+				</p>
+			</div>
+			<div>
+				<input
+					className="btn"
+					type="submit"
+					name="submit"
+					value="Log In"
+				/>
+			</div>
+		</form>
+	);
 }
