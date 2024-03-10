5683dc4d1cc491e10379c5c13e3fcb6cccad6cb4
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-03-09 23:31:07 -0600

    updated pg pool. Will say what kind of database is connected and print errors. Also tested AWS RDS,

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -6,23 +6,35 @@ let pool;
 // to set the connection info: web address, username/password, db name
 // eg:
 //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
-if (process.env.DATABASE_URL) {
-	pool = new pg.Pool({
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
+if (process.env.DATABASE_URL) { // HEROKU URL
+	try {
+		pool = new pg.Pool({
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+		});
+
+		console.log('connected to remote database');
+		// console.log('connected via DATABASE_URL: ', process.env.DATABASE_URL);
+	} catch (error) {
+		console.log('ERROR in pg pool. Cannot connect to remote database. Error:', error);
+	}
 }
 // When we're running this app on our own computer
 // we'll connect to the postgres database that is
 // also running on our computer (localhost)
 else {
-	pool = new pg.Pool({
-		host: "localhost",
-		port: 5432,
-		database: "FairShare",
-	});
+	try {
+		pool = new pg.Pool({
+			host: "localhost",
+			port: 5432,
+			database: "FairShare",
+		});
+		console.log('connected to localhost');
+	} catch (error) {
+		console.log('ERROR in pg pool. Cannot connect to localhost. Error:', error);
+	}
 }
 
 module.exports = pool;
