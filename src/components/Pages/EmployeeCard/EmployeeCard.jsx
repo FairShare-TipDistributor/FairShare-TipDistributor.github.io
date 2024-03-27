585e01b515200bfbfff0e8f590cbe65815260c67
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-03-27 02:05:45 -0500

    added navbar

--- src/components/Pages/EmployeeCard/EmployeeCard.jsx
+++ src/components/Pages/EmployeeCard/EmployeeCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 
 // import { useTheme } from '@mui/material/styles';
-import { Box, Button, TextField } from '@mui/material'; 
+import { Box, Button, TextField, Grid} from '@mui/material'; 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -25,11 +25,15 @@ const image = 'https://media.licdn.com/dms/image/D5635AQFI-fq-7L0lFQ/profile-fra
 
 function EmployeeCard(employeeProp) {
   const [expanded, setExpanded] = React.useState(false);
+  const [edit, setEdit] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
+  const toggleEdit = () => { // Toggle the EDIT useState between TRUE & FALSE. 
+      setEdit(current => !current);
+  };
 
   const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -55,12 +59,13 @@ function EmployeeCard(employeeProp) {
     <>
       <Card sx={{ display: 'flex', flexDirection: 'column', width: '100%', marginTop: .5, marginBottom: .5 }}>
       <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-      <CardMedia
+      {/* <CardMedia
         component="img"
         sx={{ width: 50, height: 50, padding: .5 }}
         image={image}
         alt="Image"
-      />
+      /> */}
+       <Avatar alt="Image" src={image} sx={{ width: 50, height: 50, margin: 1}} />
         <CardContent sx={{ flex: '1 0 auto',   padding: 1}}>
           <div>
 
@@ -94,18 +99,67 @@ function EmployeeCard(employeeProp) {
         </CardActions>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent  sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }} >
-          <div>
-          <Typography  paragraph>First Name:</Typography>
-          <TextField></TextField>
-          </div>
-          <div>
-          <Typography paragraph>Last Name:</Typography>
-          <TextField></TextField>
-          </div>
-          <div>
-          <Typography paragraph>Email:</Typography>
-          <TextField></TextField>
-          </div>
+          
+        {edit ? (
+        <Grid container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '100vw' }}>
+          <Grid  container style={{ display: 'flex', flexDirection: 'row' }}>
+            <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6}    >
+              <Typography  paragraph>First Name:</Typography>
+              <TextField value={employeeProp.employeeProp.first_name}></TextField>
+            </Grid>
+            <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6}  >
+              <Typography paragraph>Last Name:</Typography>
+              <TextField value={employeeProp.employeeProp.last_name}></TextField>
+              </Grid>
+            <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6}  >
+              <Typography paragraph>Email:</Typography>
+              <TextField value='example@gmail.com'></TextField>
+            </Grid>
+            <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6}  >
+              <Typography paragraph>Phone:</Typography>
+              <TextField value='612-888-1234'></TextField>
+            </Grid>
+            <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6}  >
+              <Typography paragraph>Job:</Typography>
+              <TextField value='server' placeholder='Server'></TextField>
+            </Grid>
+          </Grid>
+            <Grid Item  xl={12} sx={12} >
+              <Button item variant="outlined" onClick={toggleEdit}>
+                Submit Your Changes
+              </Button>
+            </Grid>
+          </Grid>
+            ) : (
+            <Grid container style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', maxWidth: '100vw' }} >
+              <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6} >
+                <Typography  paragraph>First Name: {employeeProp.employeeProp.first_name} </Typography>
+              </Grid>
+              <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6} >
+                <Typography paragraph>Last Name: {employeeProp.employeeProp.last_name}</Typography>
+              </Grid>
+              <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6} >
+                <Typography paragraph>Email: example@gmail.com</Typography>
+              </Grid>
+              <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6} >
+                <Typography paragraph>Phone: 612-888-1234</Typography>
+              </Grid>
+              <Grid Item xl={2} lg={3}  md={4} sm={5} sx={6} >
+                <Typography paragraph>Job: Server, Bartender </Typography>
+              </Grid>
+              
+              <Grid Item  xl={12} sx={12} >
+                <Button item variant="contained" onClick={toggleEdit}>
+                  Edit Profile Info
+                </Button>
+              </Grid>
+            </Grid>
+            )}
+          
+          <Button onClick={handleExpandClick} >Close</Button>
+          
+          
+         
         </CardContent>
       </Collapse>
     </Card>
